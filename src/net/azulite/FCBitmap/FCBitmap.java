c787bc1b930d1c8ffd791e2d5719c25ce906c4d0
Hiroki <1127785+azulamb@users.noreply.github.com>
2014-07-03 13:05:42 +0900

    Add JavaDoc.

--- src/net/azulite/FCBitmap/FCBitmap.java
+++ src/net/azulite/FCBitmap/FCBitmap.java
@@ -4,75 +4,45 @@ import android.graphics.Bitmap;
 
 public class FCBitmap
 {
-
+	/**
+	 * Family Computer Color Table
+	 */
 	public static int[] table =
 	{
-		0xFF000000,
-		0xFF757575,
-		0xFFBCBCBC,
-		0xFFFFFFFF,
-
-		0xFF271B8F,
-		0xFF0073EF,
-		0xFF3FBFFF,
-		0xFFABE7FF,
-
-		0xFF0000AB,
-		0xFF233BEF,
-		0xFF5F73FF,
-		0xFFC7D7FF,
-
-		0xFF47009F,
-		0xFF8300F3,
-		0xFFA78BFD,
-		0xFFD7CBFF,
-
-		0xFF8F0077,
-		0xFFBF00BF,
-		0xFFF77BFF,
-		0xFFFFC7FF,
-
-		0xFFAB0013,
-		0xFFE7005B,
-		0xFFFF77B7,
-		0xFFFFC7DB,
-
-		0xFFA70000,
-		0xFFDB2B00,
-		0xFFFF7763,
-		0xFFFFBFB3,
-
-		0xFF7F0B00,
-		0xFFCB4F0F,
-		0xFFFF9B3B,
-		0xFFFFDBAB,
-
-		0xFF432F00,
-		0xFF8B7300,
-		0xFFF3BF3F,
-		0xFFFFE7A3,
-
-		0xFF004700,
-		0xFF009700,
-		0xFF83D313,
-		0xFFE3FFA3,
-
-		0xFF005100,
-		0xFF00AB00,
-		0xFF4FDF4B,
-		0xFFABF3BF,
-
-		0xFF003F17,
-		0xFF00933B,
-		0xFF58F898,
-		0xFFB3FFCF,
-
-		0xFF1B3F5F,
-		0xFF00838B,
-		0xFF00EBDB,
-		0xFF9FFFF3,
+		0xFF000000, 0xFF757575, 0xFFBCBCBC, 0xFFFFFFFF,
+		0xFF271B8F, 0xFF0073EF, 0xFF3FBFFF, 0xFFABE7FF,
+		0xFF0000AB, 0xFF233BEF, 0xFF5F73FF, 0xFFC7D7FF,
+		0xFF47009F, 0xFF8300F3, 0xFFA78BFD, 0xFFD7CBFF,
+		0xFF8F0077, 0xFFBF00BF, 0xFFF77BFF, 0xFFFFC7FF,
+		0xFFAB0013, 0xFFE7005B, 0xFFFF77B7, 0xFFFFC7DB,
+		0xFFA70000, 0xFFDB2B00, 0xFFFF7763, 0xFFFFBFB3,
+		0xFF7F0B00, 0xFFCB4F0F, 0xFFFF9B3B, 0xFFFFDBAB,
+		0xFF432F00, 0xFF8B7300, 0xFFF3BF3F, 0xFFFFE7A3,
+		0xFF004700, 0xFF009700, 0xFF83D313, 0xFFE3FFA3,
+		0xFF005100, 0xFF00AB00, 0xFF4FDF4B, 0xFFABF3BF,
+		0xFF003F17, 0xFF00933B, 0xFF58F898, 0xFFB3FFCF,
+		0xFF1B3F5F, 0xFF00838B, 0xFF00EBDB, 0xFF9FFFF3,
 	};
 
+	/**
+	 * Create Bitmap from char array.
+	 * @param width Image width.
+	 * @param height Image height.
+	 * @param data FCBitmap string.
+	 * @return Bitmap data.
+	 */
+	public static Bitmap createBitmap( int width, int height, String data )
+	{
+		return FCBitmap.createBitmap( width, height, data.toCharArray() );
+	}
+
+	/**
+	 * Create Bitmap from char array.
+	 * @param width Image width.
+	 * @param height Image height.
+	 * @param data Char array from FCBitmap string.
+	 * @return Bitmap data.
+	 */
 	public static Bitmap createBitmap( int width, int height, char[] data )
 	{
 		int x, y, num;
@@ -101,6 +71,11 @@ public class FCBitmap
 		return bitmap;
 	}
 
+	/**
+	 * Color to char.
+	 * @param color Pixel color(Color).
+	 * @return FCBitmap Color character. Not supported color or transparent color is '_'.
+	 */
 	public static char colorChar( int color )
 	{
 		int n;
@@ -115,6 +90,11 @@ public class FCBitmap
 		return '_';
 	}
 
+	/**
+	 * Create FCBitmap string.
+	 * @param bitmap Image.
+	 * @return FCBitmap string.
+	 */
 	public static String createString( Bitmap bitmap )
 	{
 		int x, y, w, h;
